e9ec5199b2fca54e730b32d2f7668714eb6e999d
f14c0 <jcvgalfonso@gmail.com>
2013-10-18 22:20:20 -0500

    Event listeners issue fixed

--- sensor/lib/sockets.js
+++ sensor/lib/sockets.js
@@ -1,31 +1,47 @@
-var socketio = require('socket.io')
-var scan      = require('../scan.js')
+var socketio    = require('socket.io')
+var scan        = require('../scan.js')
+var bt          = new scan() 
+var _           = require("underscore") 
 
 module.exports.listen = function(app){
 	server = require('http').createServer(app)
     io = socketio.listen(server)
+    io.set('log level',3);
     server.listen(3000)
 
     //realtime namespace
     realtime = io.of('/realtime')
-
     realtime.on('connection', function(socket){
-    	var bt = new scan()
-
-    	socket.on('start_scan', function(time){
-    		bt.start(time)
-    		
-    		bt.on('device_found',function(peripheral){
-    			var btdevice =peripheral.uuid
-    			socket.emit('bt',btdevice)
-    		})
-	   	})
+        console.log('DEBUG: ' + socket.id + '  conected')
+        socket.on('start_scan', function(time){
+            //console.log("DEBUG - socket.js: star scan event emited")
+            bt.start(time)
+    		bt.on('device_found',function (peripheral){
+                var btdevice =peripheral.uuid
+                console.log('DEBUG - socket.js: device_found event has ' + _.size(bt.listeners('device_found'))+ ' listeners')
+                console.log("DEBUG - socket.js: bt device found  event emited")
+                socket.emit('bt',btdevice)
+            })
+            bt.on('scan_stopped', function(){
+                socket.emit('scan_stopped')
+                bt.removeAllListeners()
+            })
+       	})
 
     	socket.on('stop_scan', function(){
     		bt.stop()
-    	})
-
+            bt.on('scan_stopped', function(){
+                socket.emit('scan_stopped')
+                bt.removeAllListeners()
+            })
+            
+        })
+        //.removeAllListeners()
+        socket.on('disconnect', function() {
+            console.log('DEBUG: ' + socket.id + '  disconected')
+            socket.removeAllListeners()
+            //delete sockets[_.indexOf(sockets,socket.id)]
+        })
     })
-
     return io
 }
--- sensor/scan.js
+++ sensor/scan.js
@@ -1,39 +1,40 @@
-var noble = require('noble')
-var found = []
-var util = require ('util')
-var events = require('events')
+var noble     = require('noble')
+var found     = []
+var util      = require ('util')
+var events    = require('events')
 var self
+var _         = require("underscore")
 
 var scanner =function (){
   self=this
   events.EventEmitter.call(this)
+
   var timer
   this.start= function(delay){
     var time=delay
     timer=setTimeout(this.stop,1000*time)
-    noble.startScanning();
-    console.log ("Scanning..."+ 'for '+ time + ' seconds.')
+    noble.startScanning()
+    console.log ('DEBUG - Scan.js: *****Scanning...for '+ time + ' seconds.******')
     self.emit('scanning')
+    noble.on('discover', btListener)
+  }
 
-    noble.on('discover', function(peripheral) {
-      self.emit('device_found',peripheral)
-      console.log("MAC:" + peripheral.uuid)
-      console.log("Name: " + peripheral.advertisement.localName);
-      console.log("")
-      found.push(peripheral)
-    });
+  function btListener(peripheral){
+    //console.log("DEBUG - scan.js: bt device found  event emited")
+    console.log('DEBUG - scan.js: discover event has ' + _.size(noble.listeners('discover'))+ ' listeners')
+    self.emit('device_found',peripheral)
+    found.push(peripheral)
   }
 
   this.stop = function(){
     if (timer) {
-      clearTimeout(timer);
-      timer = 0;
+      clearTimeout(timer)
+      timer = 0
     }
-    noble.stopScanning();
-    console.log("Scan stopped")
+    noble.stopScanning()
+    self.emit('scan_stopped')
+    noble.removeAllListeners()
   }
 }
-
-scanner.prototype.__proto__ = events.EventEmitter.prototype;
-
+scanner.prototype.__proto__ = events.EventEmitter.prototype
 module.exports=scanner
